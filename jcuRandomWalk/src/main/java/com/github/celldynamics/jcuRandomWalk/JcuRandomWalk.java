b241c65e4ef64bf99a91829a130d42fedb604f50
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2018-05-19 11:19:06 +0100

    Added initial project for jcuRW

---
+++ jcuRandomWalk/src/main/java/com/github/celldynamics/jcuRandomWalk/JcuRandomWalk.java
@@ -0,0 +1,13 @@
+package com.github.celldynamics.jcuRandomWalk;
+
+/**
+ * Hello world!
+ *
+ */
+public class JcuRandomWalk 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
