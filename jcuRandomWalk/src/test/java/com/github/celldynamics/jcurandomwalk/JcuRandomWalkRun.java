d1821356242628502b8d9fc0794d0544cd69ac4f
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2018-05-31 15:28:35 +0100

    Changed test to runner
    To avoid executing in test mode.

---
+++ jcuRandomWalk/src/test/java/com/github/celldynamics/jcurandomwalk/JcuRandomWalkRun.java
@@ -0,0 +1,33 @@
+package com.github.celldynamics.jcurandomwalk;
+
+/**
+ * Test runner
+ * 
+ * @author baniu
+ *
+ */
+//!>
+public class JcuRandomWalkRun {
+
+ 
+  /**
+   * Main.
+   * 
+   * @param args args
+   */
+  public static void main(String[] args) {
+    // String[] args = "app -s xx -i xxx -o yy -d".split(" ");
+    // String[] args = "app -h".split(" ");
+     String[] args1 =
+     "app -i src/test/test_data/segment_test_normalised.tif -s src/test/test_data/segment_test_seeds.tif -o /tmp/solution.tif -dd"
+     .split(" ");
+
+//    String[] args =
+//            "app -i src/test/test_data/img_original_normalised.tif -s src/test/test_data/img_original_seeds.tif -o /tmp/test.tif -dd --show"
+//                    .split(" ");
+    
+    JcuRandomWalk.main(args1);
+
+  }
+  //!<
+}
