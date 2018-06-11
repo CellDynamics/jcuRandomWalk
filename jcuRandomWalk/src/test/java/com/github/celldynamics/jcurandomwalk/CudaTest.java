b3a97dd3324c89fc1604722a697a9d7d581817b7
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2018-06-11 12:18:07 +0100

    Able to run tests on CPU only (skipping)

--- jcuRandomWalk/src/test/java/com/github/celldynamics/jcurandomwalk/CudaTest.java
+++ jcuRandomWalk/src/test/java/com/github/celldynamics/jcurandomwalk/CudaTest.java
@@ -1,5 +1,7 @@
 package com.github.celldynamics.jcurandomwalk;
 
+import static org.junit.Assume.assumeTrue;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import jcuda.Pointer;
+import jcuda.jcusparse.JCusparse;
 import jcuda.runtime.JCuda;
 
 /**
@@ -22,6 +25,23 @@ public class CudaTest {
     Logger rootLogger = loggerContext.getLogger(CudaTest.class.getName());
     ((ch.qos.logback.classic.Logger) rootLogger).setLevel(Level.DEBUG);
   }
+
+  /**
+   * Check if there is cuda.
+   * 
+   * @return true if it is
+   */
+  public static boolean checkCuda() {
+    try {
+      JCusparse.setExceptionsEnabled(true);
+    } catch (Error e) {
+      return false;
+    }
+    return true;
+  }
+
+  private static final boolean isCuda = checkCuda();
+
   /**
    * The Constant LOGGER.
    */
@@ -29,6 +49,7 @@ public class CudaTest {
 
   @Test
   public void test() {
+    assumeTrue(isCuda);
     JCuda.setExceptionsEnabled(true);
     Pointer pointer = new Pointer();
     JCuda.cudaMalloc(pointer, 4);
