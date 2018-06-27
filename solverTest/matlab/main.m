2655281c6f8d8add2edd57f0c8bfc09840b7a144
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2018-06-27 10:28:52 +0100

    Updated matlab

--- solverTest/matlab/main.m
+++ solverTest/matlab/main.m
@@ -1,7 +1,10 @@
 rowIndA = importdata('/home/baniuk/Documents/jcuRandomWalk/jcuRandomWalk/data/rowInd.txt') + 1;
 colIndA = importdata('/home/baniuk/Documents/jcuRandomWalk/jcuRandomWalk/data/colInd.txt') + 1;
 valA = importdata('/home/baniuk/Documents/jcuRandomWalk/jcuRandomWalk/data/val.txt');
+<<<<<<< Updated upstream
 b = importdata('/home/baniuk/Documents/jcuRandomWalk/jcuRandomWalk/data/b.txt');
+=======
+>>>>>>> Stashed changes
 
 rowIndACOO = importdata('/home/baniuk/Documents/jcuRandomWalk/jcuRandomWalk/data/rowIndCOO.txt') + 1;
 colIndACOO = importdata('/home/baniuk/Documents/jcuRandomWalk/jcuRandomWalk/data/colIndCOO.txt') + 1;
@@ -13,6 +16,7 @@ colsA = max(colIndACOO);
 rowsA = colsA;
 nnzA = length(valACOO);
 
+<<<<<<< Updated upstream
 S = sparse(rowIndACOO, colIndACOO, valACOO, rowsA, colsA);
 
 issparse(S)
@@ -21,3 +25,6 @@ spy(S)
 tic
 x = gpuArray(S)\gpuArray(b);
 toc
+=======
+s = sparse(rowIndACOO,colIndACOO,valACOO,rowsA,colsA);
+>>>>>>> Stashed changes
