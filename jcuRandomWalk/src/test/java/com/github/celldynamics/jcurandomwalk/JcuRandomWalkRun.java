ef58c518a08d44a86aba9dcaa5bb29b39533d6eb
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2018-06-27 13:01:07 +0000

    Added cheat mode

--- jcuRandomWalk/src/test/java/com/github/celldynamics/jcurandomwalk/JcuRandomWalkRun.java
+++ jcuRandomWalk/src/test/java/com/github/celldynamics/jcurandomwalk/JcuRandomWalkRun.java
@@ -20,25 +20,26 @@ public class JcuRandomWalkRun {
     // String[] args = "app -s xx -i xxx -o yy -d".split(" ");
 //    String args1 = "app -h"; 
     
-//    String args1 =
-//           "app -i /local/internal_tmp/baniuk/rw/z_stack_deconvolved_t1.tif "
-//           + "-o /tmp/solution_small.tif "
-//           + "-dd "
-//           + "--autoth 0.95 "
-//           + "--defaultprocessing 0.4 "
-//           + "--sigmaMean 0.05 "
-//           + "--sigmaGrad 0.1 "
-//           + "--probmaps";
-//
     String args1 =
-            "app -i /local/internal_tmp/baniuk/rw/folder/small "
-            + "-o /local/internal_tmp/baniuk/rw/folder/res "
-            + "-dd "
-            + "--autoth 0.95 "
-            + "--device -2 "
-            + "--defaultprocessing 0.4 "
-            + "--sigmaMean 0.05 "
-            + "--sigmaGrad 0.1";
+           "app -i /local/internal_tmp/baniuk/rw/z_stack_deconvolved_t7.tif "
+           + "-o /home/baniuk/external/rw/folder/res/solution_t7.tif "
+           + "-dd "
+           + "--autoth 0.95 "
+           + "--defaultprocessing 0.4 "
+           + "--sigmaMean 0.05 "
+           + "--sigmaGrad 0.1 "
+           + "--probmaps";
+//
+//    String args1 =
+//            "app -i /local/internal_tmp/baniuk/rw/folder "
+//            + "-o /local/internal_tmp/baniuk/rw/folder/res "
+//            + "-dd "
+//            + "--autoth 0.95 "
+//            + "--device -2 "
+//            + "--defaultprocessing 0.4 "
+//            + "--sigmaMean 0.05 "
+//            + "--sigmaGrad 0.1 "
+//            + "--usecheating";
     
 //  String args1 =
 //          "app -i src/test/test_data/ "
